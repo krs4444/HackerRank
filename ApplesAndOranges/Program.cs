df8320fb512fcf664b11eab5de2d6583c32d9fcb
krs4444 <kristian.pinke@live.com>
2019-05-05 22:00:51 +0200

    Initial commit

---
+++ ApplesAndOranges/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace ApplesAndOranges
+{
+    class Program
+    {
+        // Complete the countApplesAndOranges function below.
+        static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            var appleCount = 0;
+            var orangeCount = 0;
+
+            foreach (var appleD in apples)
+            {
+                var appleFall = a + appleD;
+                if (appleFall >= s && appleFall <= t)
+                    appleCount++;
+            }
+
+            foreach (var orangeD in oranges)
+            {
+                var orangeFall = b + orangeD;
+                if (orangeFall >= s && orangeFall <= t)
+                    orangeCount++;
+            }
+
+            Console.WriteLine(appleCount.ToString());
+            Console.WriteLine(orangeCount.ToString());
+
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            string[] st = Console.ReadLine().Split(' ');
+
+            int s = Convert.ToInt32(st[0]);
+
+            int t = Convert.ToInt32(st[1]);
+
+            string[] ab = Console.ReadLine().Split(' ');
+
+            int a = Convert.ToInt32(ab[0]);
+
+            int b = Convert.ToInt32(ab[1]);
+
+            string[] mn = Console.ReadLine().Split(' ');
+
+            int m = Convert.ToInt32(mn[0]);
+
+            int n = Convert.ToInt32(mn[1]);
+
+            int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+            ;
+
+            int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+            ;
+            countApplesAndOranges(s, t, a, b, apples, oranges);
+        }
+    }
+}
