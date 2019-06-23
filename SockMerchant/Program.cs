a4ca7ee02e187f8be3fe3b006b71cf61d9bfa6bb
Kristian Pinke <kristian.pinke@outlook.com>
2019-06-23 23:45:57 +0200

    SockMechant

---
+++ SockMerchant/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SockMerchant
+{
+    class Program
+    {
+        // Complete the sockMerchant function below.
+        static int sockMerchant(int n, int[] ar)
+        {
+            var pairs = 0;
+            var colors = ar.Distinct();
+
+            foreach (var color in colors)
+            {
+                var colorCount = ar.Count(c => c == color);
+                if (colorCount > 0)
+                    if (colorCount % 2 == 0)
+                        pairs += colorCount / 2;
+                    else
+                        pairs += (colorCount - 1) / 2;
+            }
+
+            return pairs;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int result = sockMerchant(n, ar);
+
+            Console.WriteLine(result.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
